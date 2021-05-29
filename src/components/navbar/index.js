aea88d4690e1da7158b66634e32f55024620f23e
Daniel Maia <daniel.maia.jr12@gmail.com>
2021-05-29 16:29:53 -0300

    Added basic Navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import { AppBar, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+export default function Navbar() {
+	const [ show, setShow ] = useState(false);
+	const classes = useStyles();
+
+	return (
+		<AppBar className={classes.wrapper}>
+			<Container className={classes.container}>
+				<div>LOGO</div>
+				<div>
+					<button onClick={() => setShow(!show)}>CART</button>
+				</div>
+			</Container>
+		</AppBar>
+	);
+}
+
+const useStyles = makeStyles((theme) => ({
+	wrapper: {
+		boxShadow: 'none',
+		backgroundColor: 'black'
+	},
+	container: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		height: 80
+	}
+}));
